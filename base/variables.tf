b94160b5bac74f4d3bfe29dd7ef4badb7c06f74c
Sang Huynh <sanghuynh20000@gmail.com>
2023-09-27 07:17:22 +0700

    [#12] Add aws provider

---
+++ base/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "environment" {
+  description = "The application environment, used to tag the resources, e.g. `acme-web-staging`"
+  type        = string
+}
+
+variable "owner" {
+  description = "The owner of the infrastructure, used to tag the resources, e.g. `acme-web`"
+  type        = string
+}
