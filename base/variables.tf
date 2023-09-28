6144395b5d616b4cc327a79bb3d7afe247b25ac2
Sang Huynh <sanghuynh20000@gmail.com>
2023-09-28 07:06:49 +0700

    [#12] Integrate Terraform Cloud

--- base/variables.tf
+++ base/variables.tf
@@ -13,3 +13,13 @@ variable "owner" {
   description = "The owner of the infrastructure, used to tag the resources, e.g. `acme-web`"
   type        = string
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
