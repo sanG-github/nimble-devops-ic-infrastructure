2b6eb441e034e30e061963b84621c10455f2a52b
Sang Huynh <sanghuynh20000@gmail.com>
2023-10-03 20:20:27 +0700

    [#1] Remove redundant path

--- modules/iam/groups/main.tf
+++ modules/iam/groups/main.tf
@@ -1,19 +1,16 @@
 #tfsec:ignore:aws-iam-enforce-group-mfa
 resource "aws_iam_group" "admin" {
   name = "Admin-group"
-  path = "/admins"
 }
 
 #tfsec:ignore:aws-iam-enforce-group-mfa
 resource "aws_iam_group" "developer" {
   name = "Developer-group"
-  path = "/developers"
 }
 
 #tfsec:ignore:aws-iam-enforce-group-mfa
 resource "aws_iam_group" "bot" {
   name = "Bot-group"
-  path = "/bots"
 }
 
 resource "aws_iam_group_policy_attachment" "admin_access" {
@@ -32,4 +29,3 @@ resource "aws_iam_group_policy_attachment" "bot_power_user_access" {
   group      = aws_iam_group.bot.name
   policy_arn = data.aws_iam_policy.power_user_access.arn
 }
-
