7de82e8f4141198c0988d17f42ba5a07a78b25af
Sang Huynh <sanghuynh20000@gmail.com>
2023-10-03 17:45:17 +0700

    [#4] Remove redundant local

--- base/locals.tf
+++ base/locals.tf
@@ -5,9 +5,6 @@ locals {
   # The application environment, used to tag the resources, e.g. `acme-web-staging`
   environment = "staging"
 
-  # Sets max amount of the latest develop images to be kept
-  image_limit = 5
-
   # The owner of the infrastructure, used to tag the resources, e.g. `acme-web`
   owner = "sanghuynh20000"
 
