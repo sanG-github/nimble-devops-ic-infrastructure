ad192d9032fd2e84eaed63627ce086d2ab1fb786
Sang Huynh <sanghuynh20000@gmail.com>
2023-10-04 17:10:32 +0700

    [#4] Remove aws_secret_key and aws_access_key

--- core/variables.tf
+++ core/variables.tf
@@ -1,14 +1,3 @@
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
-
 variable "environment" {
   description = "The application environment, used to tag the resources, e.g. `acme-web-staging`"
   type        = string
