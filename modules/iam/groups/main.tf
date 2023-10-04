4419cf9897a84091ec891893b2f60628a595176c
Sang Huynh <sanghuynh20000@gmail.com>
2023-10-04 09:54:01 +0700

    [#1] User must have MFA enabled

--- modules/iam/groups/main.tf
+++ modules/iam/groups/main.tf
@@ -25,7 +25,17 @@ resource "aws_iam_group_policy_attachment" "developer_power_user_access" {
   policy_arn = data.aws_iam_policy.power_user_access.arn
 }
 
+resource "aws_iam_group_policy" "developer_allow_manage_own_credentials" {
+  group  = aws_iam_group.developer.name
+  policy = local.allow_manage_own_credentials
+}
+
 resource "aws_iam_group_policy_attachment" "bot_power_user_access" {
   group      = aws_iam_group.bot.name
   policy_arn = data.aws_iam_policy.power_user_access.arn
+}
+
+resource "aws_iam_group_policy" "bot_full_iam_access" {
+  group  = aws_iam_group.bot.name
+  policy = local.full_iam_access_policy
 }
