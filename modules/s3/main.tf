00e13a7c64f38a989919ef588494a835207862da
Sang Huynh <sanghuynh20000@gmail.com>
2023-10-05 16:36:21 +0700

    [#7] Disable tfsec rules

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,25 +1,14 @@
-resource "aws_s3_bucket" "main_bucket" {
+# tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-encryption
+resource "aws_s3_bucket" "this" {
   bucket        = local.bucket_name
   force_destroy = local.force_destroy
 }
 
-resource "aws_s3_bucket" "log_bucket" {
-  bucket        = local.log_bucket_name
-  force_destroy = local.force_destroy
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.main_bucket.id
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
 
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_logging" "this" {
-  bucket = aws_s3_bucket.main_bucket.id
-
-  target_bucket = aws_s3_bucket.log_bucket.id
-  target_prefix = "logs/main_bucket/"
 }
