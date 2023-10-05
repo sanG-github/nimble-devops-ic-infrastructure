7d2ab3ab35d12386c34d36ea6595f5f7c27d9149
Sang Huynh <sanghuynh20000@gmail.com>
2023-10-05 16:51:00 +0700

    [#7] Disable checkov rules

--- modules/s3/locals.tf
+++ modules/s3/locals.tf
@@ -2,9 +2,6 @@ locals {
   # The name of the bucket
   bucket_name = "devops-ic-bucket"
 
-  # The name of the log bucket
-  log_bucket_name = "devops-ic-log-bucket"
-
   # All objects should be deleted from the bucket when the bucket is destroyed
   force_destroy = true
 }
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,9 @@
-# tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-encryption
+# tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-bucket-encryption
 resource "aws_s3_bucket" "this" {
+  #checkov:skip=CKV2_AWS_61: There is no need to add lifecycle configuration to S3 bucket
+  #checkov:skip=CKV2_AWS_62: There is no need to add event notification to S3 bucket
+  #checkov:skip=CKV_AWS_145: There is no need to encrypt S3 bucket with KMS
+  #checkov:skip=CKV_AWS_144: There is no need to replicate S3 bucket
   bucket        = local.bucket_name
   force_destroy = local.force_destroy
 }
@@ -11,4 +15,19 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_logging" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  target_bucket = aws_s3_bucket.this.id
+  target_prefix = "logs/bucket"
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
