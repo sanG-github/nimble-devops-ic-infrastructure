44501802f717467385c378017e008659bfd08309
Sang Huynh Thanh <sanghuynh20000@gmail.com>
2023-11-03 10:08:48 +0700

    [#15] Enable failover and encryption

--- modules/elasticache/locals.tf
+++ modules/elasticache/locals.tf
@@ -6,6 +6,6 @@ locals {
   parameter_group_name       = "default.redis6.x"
   node_type                  = "cache.t2.micro"
   number_cache_clusters      = 1
-  automatic_failover_enabled = false
-  at_rest_encryption_enabled = false
+  automatic_failover_enabled = true
+  at_rest_encryption_enabled = true
 }
