57604eb887dabbfb6550a246fcc8616aaaabd1bb
Sang Huynh Thanh <sanghuynh20000@gmail.com>
2023-11-23 15:28:48 +0700

    [#17] Remove redundant locals

--- modules/bastion/locals.tf
+++ modules/bastion/locals.tf
@@ -6,13 +6,4 @@ locals {
 
   # The instance type
   instance_type = "t3.nano"
-
-  # The desired number of the instance
-  instance_desired_count = 1
-
-  # The minimum number of the instance
-  min_instance_count = 1
-
-  # The maximum number of the instance
-  max_instance_count = 1
 }
